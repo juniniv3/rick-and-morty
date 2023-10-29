cbb36600281a6d74eb73fdc59c9eab8d5463951a
Junini Florez <juniniprogramador@outlook.com>
2023-10-29 08:41:15 -0500

    fix: controlling body overflow when modal is open

--- src/app/components/utils/modal/modal.component.ts
+++ src/app/components/utils/modal/modal.component.ts
@@ -1,25 +1,36 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, OnDestroy, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.scss']
 })
-export class ModalComponent implements OnInit {
+export class ModalComponent implements OnInit , OnDestroy {
   @Output() eventEmit = new EventEmitter<String>();
   showModal: boolean = true;
   constructor() { }
 
   ngOnInit(): void {
+    this.toggleBodyHtmlOverflow(true);
   }
 
   closeModal(){
     this.showModal = false;
     this.eventEmit.emit('');
+    this.toggleBodyHtmlOverflow(false);
   }
 
   clickPreventDefault(event :MouseEvent){
     event.stopPropagation();
   }
 
+  toggleBodyHtmlOverflow(hidden: boolean = true){
+    const body = document.querySelector('body');
+    const overflowValue = hidden ? 'hidden' : '';
+    body?.style.setProperty('overflow',overflowValue)
+  }
+
+  ngOnDestroy(): void {
+    this.toggleBodyHtmlOverflow(false);
+  }
 }
